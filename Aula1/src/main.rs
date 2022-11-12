16bf7946a97ddeb36b5e93cf5146dd9ddd8bdc82
italo <italogithub@gmail.com>
2022-11-12 13:03:33 -0300

    rename project

---
+++ Aula1/src/main.rs
@@ -0,0 +1,10 @@
+
+
+fn main(){    // escopo
+    let total = 30;
+    println!("Trabalhou {} horas", total);
+
+} // fim
+// drop
+
+
--- Project/src/main.rs
+++
@@ -1,9 +0,0 @@
-
-
-fn main(){    // escopo
-    let _total = 30;
-
-} // fim
-// drop
-
-
