d16564404edec023f3763c72be7644d6f0493a35
italo <italogithub@gmail.com>
2022-11-12 14:07:47 -0300

    aprendendo sobre constantes

--- Aula1/src/main.rs
+++ Aula1/src/main.rs
@@ -1,6 +1,16 @@
+
+const SECONDS_IN_MINUTE: u32 = 60;
+
+// constantes são imutáveis e não podem ser definidas duas vezes
+
+
+
 fn main(){    // escopo
     let total: i32 = 30;
     { // escopo interno   inicio
+        // podemos definir constantes dentro ou fora do escopo a qualquer momento.
+        const MINUTES_IN_HOUR: u32 = 60;
+        const SECONDS_IN_HOUR: u32 = SECONDS_IN_MINUTE + MINUTES_IN_HOUR;
         let total = total + 20;  //conseguimos acessar o escopo externo, através do escopo interno
         println!("Trabalhou {} horas", total);
     } // fim 
