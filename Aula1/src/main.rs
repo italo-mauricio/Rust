683bf50963dc01bf5418af5c6e307a47f0f4ce52
italo <italogithub@gmail.com>
2022-11-12 13:07:42 -0300

    update

--- Aula1/src/main.rs
+++ Aula1/src/main.rs
@@ -1,10 +1,7 @@
-
-
 fn main(){    // escopo
     let total = 30;
     println!("Trabalhou {} horas", total);
 
 } // fim
 // drop
-
-
+//Rust é uma linguagem tipada
--- Aula1/target/debug/deps/Project.d
+++
@@ -1,5 +0,0 @@
-C:\Users\italo\OneDrive\Documentos\GitHub\Rust\Project\target\debug\deps\Project.exe: src\main.rs
-
-C:\Users\italo\OneDrive\Documentos\GitHub\Rust\Project\target\debug\deps\Project.d: src\main.rs
-
-src\main.rs:
