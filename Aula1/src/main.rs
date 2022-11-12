5233358cdc6ef59aba4caf8e5f7727c721f99633
italo <italogithub@gmail.com>
2022-11-12 13:17:34 -0300

    trabalhando com escopos

--- Aula1/src/main.rs
+++ Aula1/src/main.rs
@@ -1,11 +1,15 @@
 fn main(){    // escopo
-    let mut total: i32 = 30;
+    let total: i32 = 30;
+    { // escopo interno   inicio
+        let total = total + 20;  //conseguimos acessar o escopo externo, através do escopo interno
+        println!("Trabalhou {} horas", total);
+    } // fim 
     println!("Trabalhou {} horas", total);
-    total = 44;
-    println!("Trabalhou {} horas", total); 
+; 
 
 } // fim
 // drop
 //Rust é uma linguagem tipada
 // toda variável é imutavel por padrão
 // mut = mutável, transforma minha variável em mutável
+//let = inicializa uma variável
