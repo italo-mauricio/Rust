be531dbb65899fb8abb19f289aee38a964765def
italo <italogithub@gmail.com>
2022-11-12 12:02:38 -0300

    update

---
+++ Project/target/debug/Project.d
@@ -0,0 +1,1 @@
+C:\Users\italo\OneDrive\Documentos\GitHub\Rust\Project\target\debug\Project.exe: C:\Users\italo\OneDrive\Documentos\GitHub\Rust\Project\src\main.rs
---
+++ Project/target/debug/deps/Project.d
@@ -0,0 +1,5 @@
+C:\Users\italo\OneDrive\Documentos\GitHub\Rust\Project\target\debug\deps\Project.exe: src\main.rs
+
+C:\Users\italo\OneDrive\Documentos\GitHub\Rust\Project\target\debug\deps\Project.d: src\main.rs
+
+src\main.rs:
