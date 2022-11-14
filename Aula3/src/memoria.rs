91888e7002cba5c97a53a9024fd0dc55a7470cc4
italo <italogithub@gmail.com>
2022-11-13 23:12:34 -0300

    anotações

--- Aula3/src/memoria.rs
+++ Aula3/src/memoria.rs
@@ -6,25 +6,33 @@ fn main() {
     let x = 5;
     let z = true;
     let numbers = [1, 2, 3];
+
+    let users = get_users();
 }
 
 
-/* ------------------------------ MEMORY ------------------------------------
+/* ---------------------------------------------- MEMORY ----------------------------------------------------------
 
-            Content                    Size                 Lifetime                Cleaneup
+            Content                      Size                 Lifetime                     Cleaneup
 
-Static: - Program Binary              Fixed              Whole program        when program terminates
+Static: - Program Binary                 Fixed              Whole program              when program terminates
         - Static variables
         - String literals
         - knwon compilation
 
-Stack:  - Function arguments         Dynamic                Function                When function returns
-        - Local variables            Top limit
+Stack:  - Function arguments             Dynamic                Function                When function returns
+        - Local variables                Top limit
         - Each thread has an 
           isolated stack
         - Knwon compilation size
 
 
+Heap:   - Values that live beyond       Dynamic                Defined by              Manually or via GC or via RAII 
+          functions                     Up to computer         programmer or
+        - Shared across threads         limit                  language
+        - Large values
+        - Dynamic size values
+        - Unknown compilation size
 
 
 
