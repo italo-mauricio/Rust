aa629f733b02a354f7cabc2e8024411f71893519
italo <italogithub@gmail.com>
2022-11-13 21:53:30 -0300

    update

---
+++ Aula3/src/memoria.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+/* ------------------------------ MEMORY ------------------------------------
+
+            Content                 Size                 Lifetime                Cleaneup
+
+Static: - Program Binary          Fixed              Whole program        when program terminates
+        - Static variables
+        - String literals
+        - knwon compilation
+
+
+
+
+
+FREE
+
+
+
+
+
+
+
+
+
+
+
+*/
