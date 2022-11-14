c5a940f0b23830f6d00131a4e35367afe8f826b3
italo <italogithub@gmail.com>
2022-11-13 22:33:08 -0300

    anotações de memória

--- Aula3/src/memoria.rs
+++ Aula3/src/memoria.rs
@@ -1,16 +1,28 @@
+
+static _Y: u32 = 13;
+
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let z = true;
+    let numbers = [1, 2, 3];
 }
 
 
 /* ------------------------------ MEMORY ------------------------------------
 
-            Content                 Size                 Lifetime                Cleaneup
+            Content                    Size                 Lifetime                Cleaneup
 
-Static: - Program Binary          Fixed              Whole program        when program terminates
+Static: - Program Binary              Fixed              Whole program        when program terminates
         - Static variables
         - String literals
         - knwon compilation
+
+Stack:  - Function arguments         Dynamic                Function                When function returns
+        - Local variables            Top limit
+        - Each thread has an 
+          isolated stack
+        - Knwon compilation size
 
 
 
