694820668467451d46ec7935a52d877653c27253
italo <italogithub@gmail.com>
2022-11-13 21:06:37 -0300

    trabalhando com slice, array, tuplas

--- Aula2/src/tupla.rs
+++ Aula2/src/tupla.rs
@@ -1,5 +1,19 @@
 fn main(){
-    let numbers = (1, 2, 3);
-    println!("{:?}", numbers.0);   // adicionando o .0 eu pego o primeiro item da tupla
+    let mut numbers = (1, 2, 3);
+    numbers.0 = 50; // consigo adicionar um novo valor a tupla
 
+    println!("{:?}", numbers.0);   // adicionando o .0 eu pego o primeiro item da tupla
+    array();
+
+}
+
+fn array(){
+
+    let mut numbers = [1.1, 2.0, 3.3]; 
+    numbers[0] = 100.5;
+    // slice :  
+
+    println!("{:?}", &numbers[1..]); // usando esse método, consigo chamar um local especifico em um array
+
+    //usando slice, posso fatiar o array
 }
