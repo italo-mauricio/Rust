f24b4655a25c5488f3c33ca4cbaa2854a1e3a2e5
italo <italogithub@gmail.com>
2022-11-16 09:13:26 -0300

    constantes

---
+++ constantes/src/main.rs
@@ -0,0 +1,24 @@
+fn main(){
+
+    const CONST_1 :char = 'c';
+    const CONST_2 :&str = "italo";
+    const CONST_3 :u32 = 12345678;
+    const CONST_4 :bool = true, false;
+    const CONST_5 :f64 = 3.5;
+    const CONST_6 :usize = 150_500_000;
+    
+    /*
+    
+        Aprendizado sobre constantes, são variáveis que não mudam ao longo do programa, sempre serão elas mesmas do começo ao fim do código.
+    
+    
+    */
+  
+
+
+
+
+
+
+
+}
