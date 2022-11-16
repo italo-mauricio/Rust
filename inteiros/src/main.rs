4e0f78c27693204e5d3179fdaf09b7cc3d0d9379
italo <italogithub@gmail.com>
2022-11-16 06:47:10 -0300

    adicionando anotações sobre inteiros

---
+++ inteiros/src/main.rs
@@ -0,0 +1,40 @@
+/*
+
+    let salario = 231232132132145323.
+
+    let signed_8 :18 = -128;       São números entre  -128 e 127
+    let unsigned_8 :u8 = 256;      São números entre 0 e 256
+    
+
+    let signed_16 :i16 = -32768;   São números entre -32768 e 32767
+    let unsigned_16  :u16 = 65535;  São números entre 0 e 65535
+    
+    let signed_32  :i32 = -2147483648;   São números entre -2147483648 e 2147483647
+    let unsigned_32  :u32 = 4294967295;  São números entre 0 e 4294967295
+
+    let signed_64  :i64 = -9223372036854775808;  São números entre -9223372036854775808 e 9223372036854775807
+    let unsigned_63  :u64 = 18446744073709551615;  São números entre 0 e 18446744073709551615
+
+    let signed_128  :i128 =  São números entre -170141183460469231731687303715884105728 e 170141183460469231731687303715884105727
+    let unsigned_128  :u128 =  São números entre 340282366920938463463374607431768211455
+
+    let signed_arch  :isize = depende do sistema operacional
+    let unsigned_arch  :usize = depende do sistema operacional
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
