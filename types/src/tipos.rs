e7663456c34a5cc02b9a7d828fd997598c6af806
italo <italogithub@gmail.com>
2022-11-16 01:37:25 -0300

    update

---
+++ types/src/tipos.rs
@@ -0,0 +1,19 @@
+fn main(){
+
+    let tipo_char = 'c';
+    println!("O dado do tipo char é: {}", tipo_char);
+    let tipo_string = "rust language";
+    println!("O dado do tipo string é: {}", tipo_string);
+    let tipo_int = 1234;
+    println!("O dado do tipo int é: {}", tipo_int);
+    let tipo_float = 3.2;
+    println!("O dado do tipo float é: {}", tipo_float);
+    let tipo_bool = true;
+    println!("O dado do tipo booleano é: {}", tipo_bool);
+
+
+
+
+
+
+}
